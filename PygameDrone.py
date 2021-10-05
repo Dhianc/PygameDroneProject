ec21bc076d6559906013af973578d11db8299f4e
Dhianc <80415690+Dhianc@users.noreply.github.com>
2021-10-05 19:05:42 -0300

    programa principal comentado

--- PygameDrone.py
+++ PygameDrone.py
@@ -1,24 +1,25 @@
 import pygame
-from control import Control
+from controler import Controler
 import numpy as np
 import time
 
 
 pygame.init()
 
-# Definição da janela
+# Tamanho da janela
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 640
 
+#Definindo janela
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 pygame.display.set_caption("Pygame Drone")
 pygame.display.get_window_size()
 
-#background
+#Definindo imagem de fundo
 bg_image = pygame.image.load(f'images/BG.jpg')
 
 
-#set framerate
+#Configurando a taxa de atualização do jogo
 clock = pygame.time.Clock()
 FPS = 500
 count = 0
@@ -26,7 +27,7 @@ count = 0
 start_time = time.time()
 
 
-#define player action variables
+#Iniciando variáveis de ação como "false"
 esquerda = False
 direita = False
 cima = False
@@ -34,8 +35,9 @@ baixo = False
 comandar = False
 
 
-
+#Declaração da classe "drone"
 class Drone(pygame.sprite.Sprite):
+    # Caracteristicas do drone
     def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
         self.char_type = char_type
@@ -49,74 +51,54 @@ class Drone(pygame.sprite.Sprite):
         self.rect.center = (x, y)
         self.original_image = self.image
 
+    # Função que atualiza a posição do drone na tela
     def move(self, pos_x, pos_y, angle):
         self.rect.move_ip(pos_x, pos_y)
 
         self.image = pygame.transform.rotate(self.original_image, angle * 180 / np.pi)
         self.rect = self.image.get_rect(center = self.rect.center)
 
-
-    # def move(self, esquerda, direita, cima, baixo):
-    #     #reset movement variables
-    #     dx = 0
-    #     dy = 0
-    #
-    #     #assign movement variables if moving left or right
-    #     if esquerda:
-    #         dx = -self.speed
-    #     if direita:
-    #         dx = self.speed
-    #     if cima:
-    #         dy = -self.speed
-    #     if baixo:
-    #         dy = self.speed
-    #
-    #     #update rectangle position
-    #     self.rect.x += dx
-    #     self.rect.y += dy
-
-
+    # Função que desenha o drone na tela
     def draw(self):
         screen.blit(self.image, self.rect)
 
 
-
+#Declara um "drone" de nome "player"
 player = Drone('drone', 0, 0, 0.3, 1)
 
+#Define uma posição inicial
+pos_ant = np.array([0, 0])
 
-pos_last_px = np.array([0, 0])
-
-
+#Função que converte a entrada 'xa' do range x1~x2 para o range y1~y2
 def interpolate(xa, x1, x2, y1, y2):
     return ((xa - x1) / (x2 - x1) * (y2 - y1)) + y1
 
 
+controlSystem = Controler()
 
-controlSystem = Control()
-
-
+#Inicia a variável de controle que dita se o ciclo de execução deve continuar
 run = True
 while run:
-    
+
+    # Controle de fluidez
     clock.tick(FPS)
 
-   
-    screen.blit(bg_image, (0, 0))
-    player.draw()
-
+    # Atualização temporal para uso no timer
     temp = time.time() - start_time
     # print(temp)
 
-    if temp >= 26:  #tempo total dos waypoints
+    # Secção que libera o drone para pilotagem manual após ele ter percorrido os waypoints
+    if temp >= 30:
         comandar = True
 
+    # Loop que se inicia quando um evento (ação) é detectado
     for event in pygame.event.get():
 
-        #quit game
+        #Sai do jogo
         if event.type == pygame.QUIT:
             run = False
 
-        #keyboard presses
+        #Define a ação quando teclas do teclado forem pressionadas
         if comandar == True:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
@@ -131,7 +113,7 @@ while run:
                     run = False
 
 
-            #keyboard button released
+            #Atualiza o status das variáveis "esquerda, direita, cima ..." para false, quando o botão pressionado for solto
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a:
                     esquerda = False
@@ -142,20 +124,26 @@ while run:
                 if event.key == pygame.K_s:
                     baixo = False
 
+    # Define a posição do drone, baseado numa lei de controle
+    pos, angle = controlSystem.movimenta(esquerda, direita, cima, baixo, comandar)
 
-    # if not controlSystem.is_over():
-    pos_abs_m, angle = controlSystem.movimenta(esquerda, direita, cima, baixo, comandar)
-    x_abs_px = interpolate(pos_abs_m[0], -60, 25, 0, SCREEN_WIDTH)
-    y_abs_px = interpolate(pos_abs_m[1], -2, 17, 0, SCREEN_HEIGHT)
-    y_abs_px = SCREEN_HEIGHT - y_abs_px
+    # Usa a função de interpolação pra posicionar o drone no esquema de coordenadas cartesiano
+    x = interpolate(pos[0], -60, 25, 0, SCREEN_WIDTH)
+    y = interpolate(pos[1], -2, 17, 0, SCREEN_HEIGHT)
+    y = SCREEN_HEIGHT - y
             
-    pos_abs_px = np.array([int(x_abs_px), int(y_abs_px)])
-    pos_rel_px = pos_abs_px - pos_last_px
-    pos_last_px = pos_abs_px
+    pos = np.array([int(x), int(y)])
+    pos_rel = pos - pos_ant
+    pos_ant = pos
 
-    player.move(pos_rel_px[0], pos_rel_px[1], angle)
-        
+    # Atualiza o desenho do drone na tela
+    player.move(pos_rel[0], pos_rel[1], angle)
 
+    # Atualização das imagems de fundo e do drone
+    screen.blit(bg_image, (0, 0))
+    player.draw()
+
+    # Atualiza a tela
     pygame.display.update()
 
 pygame.quit()
